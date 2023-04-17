ba45d2edea6b258233f7170fd281dff13a41d2fd
dnyaneshwar015 <dnyaneshwarbhlerao39@gmail.com>
2023-04-17 15:58:58 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,43 @@
+//single line comment
+/* multi line comment */
+
+/*
+alert("Congratulation!")
+alert("Hello")
+alert(4+6);
+let num=15;
+    alert(num)
+var x = 100;
+    alert(x)
+const y = 16; 
+    alert(y)
+y=17;
+ alert(y)
+*/
+
+/*
+    let a=30
+    a*=5
+    alert(a)
+
+//relational operator
+    var res=12==0;
+    alert(res)
+*/
+
+//logical operator
+    var x = 10;
+    var y = 5;
+    alert(x==y && x>y)
+
+//condition statement
+if(x>y)
+    alert("Dnyaneshwar")
+else
+    alert("Bhalerao")
+
+//type
+    alert(typeof(true));
+
+    let age=prompt("what is your age:")
+    alert("your age is age "+age)
